671cd3acc3696b553977f1f59ba2eea9bc90d65a
Arashk Asadian <arashkasadian0@gmail.com>
2024-03-27 12:03:36 +0330

    add-cards-admin
    now admin can delete and add cards

--- src/app/admin/layouts/sidebar.jsx
+++ src/app/admin/layouts/sidebar.jsx
@@ -1,12 +1,23 @@
+'use client'
 import React from 'react';
 import Link from 'next/link';
+import { handleSignOut } from '@/lib/actions';
+import { usePathname } from 'next/navigation'
+
 const Sidebar = () => {
+    const path = usePathname();
+
     return (
-        <div className='w-1/5  flex  flex-col justify-center items-center  space-x-2 space-y-3 space-x-reverse'>
-            <Link href='/login' className="text-slate-100 bg-blue-700 font-extrabold rounded-2xl sm:w-[150px] sm:h-[90px] w-full py-5 text-sm lg:text-base shadow-gray-400 shadow-md flex justify-center items-center hover:bg-blue-500 transition-colors ">ثبت سفارش </Link>
-            <Link href='/signup' className="text-indigo-950 bg-slate-100 font-extrabold rounded-2xl sm:w-[150px] sm:h-[90px] w-full py-5 text-sm lg:text-base shadow-gray-400 shadow-md flex justify-center items-center hover:bg-blue-500 hover:text-white transition-colors ">خارج شدن</Link>
+        <div className='lg:w-1/5 w-full flex lg:flex-col justify-center items-center lg:space-y-5 space-x-2 lg:space-x-0 space-x-reverse'>
+            <Link href='/admin/allorders' className={`${path == '/admin/allorders' ? "text-slate-100 bg-blue-700" : "text-indigo-950 bg-slate-100"} font-extrabold rounded-2xl sm:w-[154px] sm:h-[94px] w-full py-5 px-3 sm:px-0 sm:py-0 text-sm sm:text-base shadow-gray-400 shadow-md flex justify-center items-center hover:bg-blue-500 hover:text-white transition-colors `}>سفارشات</Link>
+            <Link href='/admin/cards' className={`${path == '/admin/cards' ? "text-slate-100 bg-blue-700" : "text-indigo-950 bg-slate-100"} text-nowrap  font-extrabold rounded-2xl sm:w-[154px] sm:h-[94px] w-full py-5 px-3 sm:px-0 sm:py-0 text-sm sm:text-base shadow-gray-400 shadow-md flex justify-center items-center hover:bg-blue-500 hover:text-white transition-colors`}>کارت‌ها</Link>
+            <form action={handleSignOut} className='text-indigo-950 text-nowrap bg-slate-100 font-extrabold rounded-2xl sm:w-[154px] sm:h-[94px] w-full py-5 px-3 sm:px-0 sm:py-0 text-sm sm:text-base shadow-gray-400 shadow-md flex justify-center items-center hover:bg-rose-500 hover:text-white transition-colors '>
+                <button className='w-full h-full'>خروج از حساب </button>
+            </form>
         </div>
     );
 }
 
 export default Sidebar;
+
+
---
+++ src/components/dashboard/cardshandle/allcardstable.jsx
@@ -0,0 +1,101 @@
+'use client'
+import axios from '@/lib/axios';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'sonner';
+
+const Allcardstable = () => {
+
+    const [cartsData, setCartsData] = useState([])
+
+    useEffect(() => {
+        try {
+            axios.get('/sanctum/csrf-cookie').then(async () => {
+                const response = await axios.get('api/carts/show');
+                if (response.status === 200) {
+                    // Handle response if necessary
+                    setCartsData(response.data)
+                }
+            })
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }, []);
+
+    const handleDeleteCard = (item) => {
+
+        try {
+            axios.get('/sanctum/csrf-cookie').then(async () => {
+                const response = await axios.delete(`api/carts/destroy/${item.id}`);
+                if (response.status === 200) {
+                    toast(response.data.message, {
+                        classNames: {
+                            toast: 'text-rose-500',
+                        },
+                    });
+
+                    setCartsData(cartsData.filter(c => c.id !== item.id));
+                }
+            })
+
+        } catch (error) {
+            toast(error, {
+                classNames: {
+                    toast: 'text-rose-500',
+                },
+            });
+        }
+    }
+
+
+
+    return (
+
+        <div className="overflow-x-auto w-full rounded-3xl">
+            <table className="w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50 ">
+                    <tr>
+                        <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">ردیف</th>
+                        <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">نام</th>
+                        <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">توضیحات</th>
+                        <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">عنوان</th>
+                        <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">ویرایش</th>
+                        <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">حذف</th>
+                    </tr>
+                </thead>
+                <tbody className="bg-white overflow-scroll divide-gray-200">
+                    {cartsData.map((item) => (
+                        <tr key={item.id}>
+                            <td className="px-6 py-4 text-center text-sm whitespace-nowrap">{item.id}</td>
+                            <td className="px-6 py-4 text-center text-sm whitespace-nowrap">{item.name}</td>
+                            <td className="px-6 py-4 text-center text-sm whitespace-nowrap">{item.description}</td>
+                            <td className="px-6 py-4 text-center text-sm whitespace-nowrap">{item.title}</td>
+                            <td className="px-6 py-4 text-center text-sm whitespace-nowrap">
+                                <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-400">
+                                    ادیت
+                                </button>
+                            </td>
+                            <td className="px-6 py-4 text-center text-sm whitespace-nowrap">
+                                <button onClick={() =>
+                                    toast.custom((t) => (
+                                        <div className='bg-slate-100 shadow px-5 py-4 text-slate-800 rounded-md flex items-center space-x-4 space-x-reverse'>
+                                            <span className='font-bold'>آیا اطمینان دارید ؟ </span>
+                                            <button className='px-5 py-2 text-xs bg-rose-500 text-slate-100 rounded-md' onClick={() => toast.dismiss(t)}>خیر</button>
+                                            <button className='px-5 py-2 text-xs bg-lime-500 text-slate-100 rounded-md' onClick={() => {handleDeleteCard(item);toast.dismiss(t)}}>بله</button>
+                                        </div>
+                                    ))}
+                                    className="bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-red-400">
+                                    حذف
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+
+    );
+};
+
+export default Allcardstable;
