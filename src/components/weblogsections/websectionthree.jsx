cc4afe2ad017b880ec02fe2add7d665627b056ad
kamran <ka6471381@gmail.cm>
2024-05-07 22:41:51 +0330

    more comment section pages

--- src/components/weblogsections/websectionthree.jsx
+++ src/components/weblogsections/websectionthree.jsx
@@ -81,13 +81,13 @@ function Websectionthree() {
                             </div>
                             <span className='pr-3'>fearles x</span>
                         </div>
-                        <div className='lg:w-4/5 relative pr-3 flex items-center '>
+                        <div className='lg:w-4/5 w-full relative pr-3 flex items-center '>
 
                             <input
                                 type="text"
                                 id="inputField"
                                 name="inputField"
-                                className="w-full px-4 py-4 border rounded-md focus:outline-none focus:border-blue-400 border-r-2  border-r-[#0045CE] "
+                                className=" w-[100%] px-4 py-4 border rounded-md focus:outline-none focus:border-blue-400 border-r-2  border-r-[#0045CE] "
                                 placeholder="سوال یا نظری وارد کنید..."
                             />
 
@@ -116,35 +116,80 @@ function Websectionthree() {
                     <div class="py-2">
                         <div class="flex flex-col items-center space-y-2">
                             {profilepics.map((item) => (
-                                <div key={item.id} className="flex items-center w-full lg:w-2/4 h-28 bg-slate-50 rounded-md relative overflow-hidden">
-                                    <div className="w-14 h-14 mr-2 relative">
-                                        <Image
-                                            src={item.imageUrl}
-                                            className="object-cover rounded-full"
-                                            layout="fill"
-                                            alt="Profile Image"
-                                        />
+                                <>
+                                    <div key={item.id} className=" w-full lg:w-2/4 h-40 bg-slate-50 rounded-md relative overflow-hidden">
+                                        <div className=' border-r-2  border-r-[#0045CE] h-full'>
+
+
+                                            <div className='flex flex-col px-3 py-3'>
+                                                <div className='flex items-center gap-2'>
+                                                    <div className="  w-14 h-14 mr-2 relative px-6  ">
+                                                        <Image
+                                                            src={item.imageUrl}
+                                                            className="object-cover rounded-full"
+                                                            layout="fill"
+                                                            alt="Profile Image"
+                                                        />
+                                                    </div>
+                                                    <span className='text-sm'>fearles x</span>
+                                                </div>
+
+
+                                                <div className="px-2  flex flex-col">
+                                                    <span className="text-sm">{item.title}</span>
+                                                    <span className="text-sm">{item.title}</span>
+                                                    <span className="text-sm">{item.title}</span>
+                                                    <span className="text-sm">{item.title}</span>
+
+
+                                                </div>
+
+                                            </div>
+                                            <div className=' absolute top-0 left-0 flex items-center px-3 py-3'>
+                                                <div className='flex gap-1 px-2'>
+                                                    <Likebutton />
+                                                    <Dislikebutton />
+                                                </div>
+
+                                                <div className='absolute top-8 left-6'>
+                                                    <span className='text-sm text-[#C00000]'>20</span>
+                                                </div>
+                                                <div className='absolute top-8 left-12'>
+                                                    <span className='text-sm'>20</span>
+                                                </div>
+                                            </div>
+
+                                            <div className="absolute bottom-0 left-1 flex items-center space-x-2">
+                                                <span className="text-xs text-slate-500 px-2">بیشتر...</span>
+                                                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                                    <span className="text-xs">پاسخ</span>
+                                                </button>
+                                            </div>
+
+
+
+
+                                        </div>
                                     </div>
-                                    <div className="px-2 flex-grow">
-                                        <span className="text-sm">{item.title}</span>
-                                    </div>
-                                    <div className="absolute bottom-0 left-1 flex items-center space-x-2">
-                                        <span className="text-xs text-slate-500 px-2">بیشتر...</span>
-                                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                            <span className="text-xs">پاسخ</span>
-                                        </button>
-                                    </div>
-                                </div>
+
+                                </>
                             ))}
+
+
+                        </div>
+                        <div className='flex justify-end mt-10'>
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                <span className="text-md">بیشتر </span>
+                            </button>
                         </div>
                     </div>
                 </div>
 
                 <div class="w-full lg:w-1/2">
                     <div class="py-2">
-                        <div class="flex flex-col items-start space-y-2">
-                            <div class="flex items-start pt-3 lg:w-3/4 h-44 bg-slate-50 rounded-md relative overflow-hidden">
-                                <div className='lg:w-2/3 '>
+                        <div class=" space-y-2">
+                            <div class="flex items-start pt-3 lg:w-3/4 w-full h-44 bg-slate-50 rounded-md relative overflow-hidden">
+                                <div className='lg:w-2/3 pr-3 '>
                                     <div style={{ position: 'relative', width: '3.5rem', height: '3.5rem' }}>
                                         <Image
                                             src='/assets/images/weblog header/joshua-aragon-EaB4Ml7C7fE-unsplash.jpg'
@@ -153,7 +198,7 @@ function Websectionthree() {
                                             alt='Post Image'
                                         />
                                     </div>
-                                    <div class="flex flex-col">
+                                    <div class="flex flex-col ">
                                         {profilepics.map(item => (
                                             <span className='text-sm'>{item.title}</span>
                                         ))}
@@ -176,17 +221,36 @@ function Websectionthree() {
 
                             </div>
 
-                            <div class="relative items-center  lg:w-3/4  bg-slate-50 rounded-md mt-2 p-2 ">
+                            <div class=" items-center  lg:w-3/4 w-full  bg-slate-50 rounded-md mt-2 p-2 relative ">
+
                                 <input
                                     type="text"
                                     id="inputField"
                                     name="inputField"
-                                    className="  w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-400"
+                                    className=" w-[100%] px-4 py-2 rounded-l-full focus:outline-none focus:border-blue-400 border-r-2  border-r-[#0045CE] h-full "
                                     placeholder="سوال یا نظری وارد کنید..."
                                 />
+                                <div className='absolute top-5 left-5'>
+                                    <svg width="28" height="25" viewBox="0 0 46 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M15.022 16.5311L15.597 35.0749L46.0001 1.36548L15.022 16.5311Z" fill="url(#paint0_linear_469_15)" />
+                                        <path d="M0 2.5875L14.4469 15.1656L45.4969 0L0 2.5875Z" fill="url(#paint1_linear_469_15)" />
+                                        <defs>
+                                            <linearGradient id="paint0_linear_469_15" x1="16.2718" y1="70.033" x2="75.2902" y2="-7.59895" gradientUnits="userSpaceOnUse">
+                                                <stop stop-color="#0045CE" />
+                                                <stop offset="1" stop-color="#00FFF0" />
+                                            </linearGradient>
+                                            <linearGradient id="paint1_linear_469_15" x1="1.83553" y1="30.8931" x2="14.0097" y2="-21.3837" gradientUnits="userSpaceOnUse">
+                                                <stop stop-color="#0045CE" />
+                                                <stop offset="1" stop-color="#00FFF0" />
+                                            </linearGradient>
+                                        </defs>
+                                    </svg>
+
+                                </div>
                             </div>
 
                         </div>
+
                     </div>
                 </div>
 
